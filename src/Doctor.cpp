ab2543961a64d2f72541d21260d0974ced5f13da
Raha-Nureen-Nesa <rnureennesa@gmail.com>
2026-05-31 14:46:56 +0900

    Update Doctor.cpp

---
+++ src/Doctor.cpp
@@ -0,0 +1,53 @@
+#include "../include/Doctor.h"
+
+// Empty values are used when no doctor details are given.
+Doctor::Doctor()
+    : User(), specialization(""), availableDay(""), availableTime("") {
+}
+
+// This constructor is used when we create a doctor with full details.
+Doctor::Doctor(int id, string name, string phone, string email,
+               string specialization, string availableDay, string availableTime)
+    : User(id, name, phone, email),
+      specialization(specialization),
+      availableDay(availableDay),
+      availableTime(availableTime) {
+}
+
+string Doctor::getSpecialization() const {
+    return specialization;
+}
+
+string Doctor::getAvailableDay() const {
+    return availableDay;
+}
+
+string Doctor::getAvailableTime() const {
+    return availableTime;
+}
+
+void Doctor::setSpecialization(string newSpecialization) {
+    specialization = newSpecialization;
+}
+
+void Doctor::setAvailableDay(string newAvailableDay) {
+    availableDay = newAvailableDay;
+}
+
+void Doctor::setAvailableTime(string newAvailableTime) {
+    availableTime = newAvailableTime;
+}
+
+void Doctor::displayInfo() const {
+    cout << "Doctor ID: " << id << endl;
+    cout << "Name: " << name << endl;
+    cout << "Phone: " << phone << endl;
+    cout << "Email: " << email << endl;
+    cout << "Specialization: " << specialization << endl;
+    cout << "Available Day: " << availableDay << endl;
+    cout << "Available Time: " << availableTime << endl;
+}
+
+string Doctor::getRole() const {
+    return "Doctor";
+}
